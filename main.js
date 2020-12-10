e661b77b8289c3127ad049ab77bbe3dedef2b96d
Ivana Andersson <ivana.p.kostova@gmail.com>
2020-12-10 14:44:16 +0200

    complete lesson 8 Computer properties

--- main.js
+++ main.js
@@ -3,15 +3,25 @@ const app = Vue.createApp({
     return {
       cart: 0,
       product: "Socks",
-      image: "./assets/images/socks_blue.jpg",
+      brand: "Vue Mastery",
+      selectedVariant: 0,
       url: "https://google.com",
       inventory: 100,
-      inStock: true,
       onSale: true,
       details: ["50% cotton", "30% wool", "20% polyester"],
       variants: [
-        { id: 1234, color: "green", image: "./assets/images/socks_green.jpg" },
-        { id: 1235, color: "blue", image: "./assets/images/socks_blue.jpg" },
+        {
+          id: 1234,
+          color: "green",
+          image: "./assets/images/socks_green.jpg",
+          quantity: 50,
+        },
+        {
+          id: 1235,
+          color: "blue",
+          image: "./assets/images/socks_blue.jpg",
+          quantity: 0,
+        },
       ],
       sizes: [7, 7.5, 8, 8.5],
     };
@@ -30,8 +40,23 @@ const app = Vue.createApp({
         this.inventory += 1;
       }
     },
-    changeImage(variantImage) {
-      this.image = variantImage;
+    updateVariant(index) {
+      this.selectedVariant = index;
+    },
+  },
+
+  computed: {
+    title() {
+      return this.brand + " " + this.product;
+    },
+    image() {
+      return this.variants[this.selectedVariant].image;
+    },
+    inStock() {
+      return this.variants[this.selectedVariant].quantity;
+    },
+    isOnSale() {
+      return this.brand + " " + this.product + " is on sale!";
     },
   },
 });
